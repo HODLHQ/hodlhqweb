5a95cec1fb4eb046a2756c2671c1cf844d12a44b
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-06-17 02:48:45 +0100

    working

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import discordLogo from "public/icons/discord.png";
 import instagramLogo from "public/icons/instagram.png";
 import ConnectButton from "components/WeBrew3/ConnectButton";
 import {useState, useEffect} from "react";
-
+import Link from "next/link";
 const HeaderNav = () => {
   const [innerHeight, setInnerHeight] = useState(0);
   const [innerWidth, setInnerWidth] = useState(globalThis.innerWidth);
@@ -26,23 +26,23 @@ const HeaderNav = () => {
       </a>
       
       <div className={"flex text-[#74d9ff] font-bold uppercase items-center space-x-5 " + (innerHeight > innerWidth ? "ml-1" : "ml-5")}>
-      <a href="https://twitter.com/HODLHQ" target="_blank" className={socialIconClass}><Image src={twitterLogo} layout="" objectFit="contain"/></a>
-      <a href="https://discord.gg/hodlhq" target="_blank" className={socialIconClass}><Image src={discordLogo} layout="" objectFit="contain" /></a>
-      <a href="https://www.instagram.com/hodlhq/" target="_blank" className={socialIconClass}><Image src={instagramLogo} layout="" objectFit="contain" /></a>
+      <a href="https://twitter.com/HODLHQ" target="_blank" rel="noreferrer" className={socialIconClass}><Image src={twitterLogo} layout="" objectFit="contain"/></a>
+      <a href="https://discord.gg/hodlhq" target="_blank" rel="noreferrer" className={socialIconClass}><Image src={discordLogo} layout="" objectFit="contain" /></a>
+      <a href="https://www.instagram.com/hodlhq/" target="_blank" rel="noreferrer" className={socialIconClass}><Image src={instagramLogo} layout="" objectFit="contain" /></a>
       </div>
       <div className={"w-[200px] h-full relative flex items-center justify-start " + (mobile ? "" : "hidden")}><div className="ml-7 absolute"><ConnectButton/></div></div>
       <div className={"flex-1 flex text-[#74d9ff] font-bold uppercase items-center justify-end space-x-10 " + (mobile ? "text-[10px] justify-center" : "")}>
-        <a href="/gallery">Gallery</a>
+        <Link href="/gallery">Gallery</Link>
         <div className="text-[#AAA]">Vault</div>
-        <a target="_blank" href="https://hodlhq.io">Shop</a>
+        <a target="_blank" rel="noreferrer" href="https://hodlhq.io">Shop</a>
         <div>OGs</div>
         <div className="dropdown">
-        {mobile ? <a href="/collections">Collections</a> : <a>Collections</a>}
+        {mobile ? <Link href="/collections">Collections</Link> : <a>Collections</a>}
           <div className="dropdown-content">
-              <a target="_blank" href="https://coolxclones.xyz">COOL X CLONES</a><br/>
-              <a target="_blank" href="https://market.decentraland.org/accounts/0x6005cdca3e6aef6aa9ff5a6301b09c770aa55783?assetType=item&section=wearables">DIAMOND WEARABLE</a><br/>
-              <a target="_blank" href="https://opensea.io/collection/podmarket">POD MARKET</a><br/>
-              <a target="_blank" href="https://juicecrewnft.com">JUICE CREW</a><br/>
+              <a target="_blank" rel="noreferrer" href="https://coolxclones.xyz">COOL X CLONES</a><br/>
+              <a target="_blank" rel="noreferrer" href="https://market.decentraland.org/accounts/0x6005cdca3e6aef6aa9ff5a6301b09c770aa55783?assetType=item&section=wearables">DIAMOND WEARABLE</a><br/>
+              <a target="_blank" rel="noreferrer" href="https://opensea.io/collection/podmarket">POD MARKET</a><br/>
+              <a target="_blank" rel="noreferrer" href="https://juicecrewnft.com">JUICE CREW</a><br/>
           </div>
         </div>
         {/*<div>Dashboard</div>*/}
--- pages/collections.js
+++ pages/collections.js
@@ -16,10 +16,10 @@ const Collections = (props) => {
       <div className="w-full container m-0 h-full flex flex-col">
         <Navbar />
         <div className="w-full h-full flex flex-col items-center justify-evenly text-[#74d9ff] font-bold uppercase text-[30px] ">
-          <a target="_blank" href="https://coolxclones.xyz">COOL X CLONES</a><br/>
-          <a target="_blank" href="https://market.decentraland.org/accounts/0x6005cdca3e6aef6aa9ff5a6301b09c770aa55783?assetType=item&section=wearables">DIAMOND WEARABLE</a><br/>
-          <a target="_blank" href="https://opensea.io/collection/podmarket">POD MARKET</a><br/>
-          <a target="_blank" href="https://juicecrewnft.com">JUICE CREW</a><br/>
+          <a target="_blank" rel="noreferrer" href="https://coolxclones.xyz">COOL X CLONES</a><br/>
+          <a target="_blank" rel="noreferrer" href="https://market.decentraland.org/accounts/0x6005cdca3e6aef6aa9ff5a6301b09c770aa55783?assetType=item&section=wearables">DIAMOND WEARABLE</a><br/>
+          <a target="_blank" rel="noreferrer" href="https://opensea.io/collection/podmarket">POD MARKET</a><br/>
+          <a target="_blank" rel="noreferrer" href="https://juicecrewnft.com">JUICE CREW</a><br/>
         </div>
         
       </div>
